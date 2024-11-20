78cba3194941629d643769644f848db29a5efb46
Assa <codermasochist@gmail.com>
2024-11-20 18:08:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-document.getElementById("button").addEventListener("click", function() {
-    const animation = document.getElementById("animation");
-    const button = document.getElementById("button");
-
-    // Скрыть кнопку
-    button.style.display = "none";
-
-    // Показать анимацию
-    animation.classList.remove("hidden");
-    const letters = animation.querySelectorAll(".name");
-
-    // Запустить анимацию поочередно для каждой буквы
-    letters.forEach((letter, index) => {
-        setTimeout(() => {
-            letter.style.opacity = "1";
-            letter.style.animationDelay = `${index * 0.3}s`;
-        }, index * 300);
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.querySelector('.confession').style.opacity = 1;
+    }, 2000);
 });
